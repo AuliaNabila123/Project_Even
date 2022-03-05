e75ede81d6ecc3617e049d865e477b5d24027cb4
nabila realisti <aulia.nabila.ft20@mail.umy.ac.id>
2022-03-05 22:57:20 +0700

    Activity Membuat Even 1

---
+++ app/src/main/java/com/example/projecteven/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.projecteven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Deklarasi variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi varabel untuk menyimpan email dan password
+    String nama, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada Layout
+        btnLogin=findViewById(R.id.btSignin);
+
+        //Menghubungkan variabel edemail dengan componen button pada Layout
+        edemail=findViewById(R.id.edEmail);
+
+        //Menghubungkan variabel edpassword dengan componen button pada Layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //Membuat fungsi onclik pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Menyimpan input user di edittext email kedalam variabel nama
+                nama = edemail.getText().toString();
+
+                //Menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+                //Membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "email anda: "+nama+" dan Password anda: "+password+"", Toast.LENGTH_LONG);
+                //Menampilkan toast
+                t.show();
+            }
+        });
+    }
+}
